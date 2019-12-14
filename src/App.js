caf90d12c0b5d199d391ab1c4d10db2ba55e96cb
Wagner Reis <wagnerreis1997@gmail.com>
2019-12-14 15:17:39 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import useGet from './useGet'
+
+const url = "https://mymoney-react.firebaseio.com/movimentacoes.json";
 
 function App() {
+  const data = useGet(url)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Loading...</p>}
     </div>
   );
 }
