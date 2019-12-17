825469560cdbe4f9c6f1c9839cda220d6514696e
Wagner Reis <wagnerreis1997@gmail.com>
2019-12-17 18:29:20 -0300

    Componentização

---
+++ src/elements/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className='navbar navibar-light bg-ligh'>
+            <div className='container'>
+                <a className='navbar-brand'>MyMoney</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
