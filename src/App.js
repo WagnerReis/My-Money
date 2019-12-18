48daf88a84750c3f3a42d0869fe5e46d6ec455cd
Wagner Reis <wagnerreis1997@gmail.com>
2019-12-18 15:06:01 -0300

    Iniciando navegação

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from "react"
-import Rest from './rest'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './elements/Header'
-import Meses from './Meses'
-import AdicionarMes from './AdicionarMes'
-
-const baseURL = 'https://mymoney-react.firebaseio.com/'
-
-const { useGet, usePost, useDelete } = Rest(baseURL)
+import Home from './pages/Home/Index'
+import Movimentacoes from './pages/Movimentacoes'
 
 function App() {
-  //const data = useGet('movimentacoes/2019-08')
-  const data = useGet('meses')
-  //const [postData, post] = usePost('movimentacoes/2019-08')
-  //const [deleteData, remove] = useDelete()
-
-  const saveNew = () => {
-    //post({ valor: 10, descricao: 'olá'})
-  }
-  const doRemove = () => {
-    //remove('movimentacoes/-Lw4eKHcTzrSqxn-XS1Z')
-  }
-
   return (
-    <div>
-      <div className='container'>
-        <AdicionarMes />
-        <Meses />
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/movimentacoes/:data' component={Movimentacoes} />
       </div>
-    </div>
+    </Router>
   );
 }
 
